9737a86fd79f6878bd2e1465d6b2dcd78d666302
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2021-10-23 19:09:53 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,14 +1,14 @@
 import math
 def calculateStats(numbers):
     computedStats = {}
-if numbers == []:
-    computedStats["min"] = float('nan')
-    computedStats["max"] = float('nan')
-    computedStats["avg"] = float('nan')
-else:
-    computedStats["min"] = min(numbers)
-    computedStats["max"] = max(numbers)
-    computedStats["avg"] = sum(numbers)/len(numbers)
+    if numbers == []:
+        computedStats["min"] = float('nan')
+        computedStats["max"] = float('nan')
+        computedStats["avg"] = float('nan')
+    else:
+        computedStats["min"] = min(numbers)
+        computedStats["max"] = max(numbers)
+        computedStats["avg"] = sum(numbers)/len(numbers)
     
 class EmailAlert:
     def __init__(self):
