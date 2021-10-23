5b5ce8bacb364429ecef8db02ce611624827e03b
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2021-10-23 19:06:19 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -10,20 +10,13 @@ else:
     computedStats["max"] = max(numbers)
     computedStats["avg"] = sum(numbers)/len(numbers)
     
-   
-    
-        
-       
-        
-        
-
 class EmailAlert:
     def __init__(self):
         self.emailSent = False
- class LEDAlert:
+class LEDAlert:
     def __init__(self):
         self.ledGlows = False
- class StatsAlerter:
+class StatsAlerter:
     def __init__(self, maxThreshold, alert):
         self.maxThreshold = maxThreshold
         self.alert = alert
@@ -33,13 +26,7 @@ class EmailAlert:
                 self.alert[0].emailSent = True
                 self.alert[1].ledGlows = True
                 break
-emailAlert = EmailAlert()
-ledAlert   = LEDAlert()
-maxThreshold = 10.5
-statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
-statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
-print(ledAlert.ledGlows)
-print(emailAlert.emailSent)
+
        
         
     
