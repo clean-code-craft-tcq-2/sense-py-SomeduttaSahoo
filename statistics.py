62056e52501dea8b93d287d82a59f9a2ac22e9f7
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2021-10-23 18:40:37 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,14 +1,16 @@
 import math
 def calculateStats(numbers):
     computedStats = {}
-    if numbers == []:
-        computedStats["min"] = float('nan')
-        computedStats["max"] = float('nan')
-        computedStats["avg"] = float('nan')
-     else:
-        computedStats["min"] = min(numbers)
-        computedStats["max"] = max(numbers)
-        computedStats["avg"] = sum(numbers)/len(numbers)
+if numbers == []:
+    computedStats["min"] = float('nan')
+    computedStats["max"] = float('nan')
+    computedStats["avg"] = float('nan')
+else:
+    computedStats["min"] = min(numbers)
+    computedStats["max"] = max(numbers)
+    computedStats["avg"] = sum(numbers)/len(numbers)
+    
+   
     
         
        
